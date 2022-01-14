ae3bdbeb443304013c61241ebbe4d70d8abc1f2c
DiscPorridge <97075073+DiscoPorridge@users.noreply.github.com>
2022-01-14 02:35:04 +0000

    day 3

--- Day 2/tipcalculator.py
+++ Day 2/tipcalculator.py
@@ -9,24 +9,38 @@
 
 # ask price > tip precent > group size > calculate split > print split
 
+###### Welcome Message and User Inputs
+
 print("Welcome to tip calculator ver 0.01")
+
 price = round(float(input("How much is the bill? ")), 2) #round to 2 in case of user typo
 
-tip_percent = float(input("Do you want to tip 10, 12 or 15%? ")) #implement error on wrong entered number
+tip_percent = int(input("Do you want to tip 10, 12 or 15%? ")) #implement error on wrong entered number
 
-group_size = float(input("How many people is it being split with? ")) #only allow integer
+group_size = int(input("How many people is it being split with? ")) #only allow integer
 
 
 # (total / people) * tip_percent
 
 # total = float((price / group_size) * float(1.0 + tip_percent))
 
+########## Calculations
 
 tip_tester = price * tip_percent / group_size / 100
 
-total_price = price / group_size + tip_tester
+total_price = "{:.2F}".format(price / group_size + tip_tester)
 
-print(f"Each person should pay: ${round(total_price, 2)}")
+# total_price_rounded = "{:.2f}".format(total_price)
+
+bill_with_tip = (tip_percent / 100 * price + price) / group_size
+
+# print(f"Each person should pay: ${total_price_rounded}")
+
+########### Final Result and Output
+
+print(f"Each person should pay: ${round(bill_with_tip,2)}")
+
+print(f"Each person should pay: ${total_price}")
 
 # print(f"the bill split is {split}")
 # print(f"the tip amount is {amount}")
