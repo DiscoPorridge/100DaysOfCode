e347e022beb45cdd01826db2186694f227df48db
DiscPorridge <97075073+DiscoPorridge@users.noreply.github.com>
2022-01-17 02:57:52 +0000

    day 3 stuff

---
+++ Day 3/day3scratchpad.py
@@ -0,0 +1,44 @@
+######## Tickets #######
+
+# blah = int(input("blah blah"))
+
+# if(blah == 120):
+#     print("blah blah!")
+# elif blah == 69:
+#     print("elif")
+# else:
+#     print("blah :(")
+
+######## Odd/Even #######
+
+n = int(input("gimme numbers to nom nom"))
+
+a = int(input("age?"))
+
+# if n % 2 > 0:
+#     print("I've had enough heres a number back")
+# else:
+#     print("that was the perfect amount of numbers!")
+
+# if n >= 120 + a >= 18:
+#     print("you are taller than 120cm")
+
+# print(n & a) ???
+
+# print(n and a) why does it only print one of the variables
+# print(a and n) 
+
+
+# elif n >= 120 & a < 18:
+#   print("You are tall enough and younger than 18, tickets cost $7")
+
+# if n >= 120 and 
+
+if n >= 120 and a >= 18:
+    print("You are tall enough to ride the rollercoaster, A ticket will cost you £12")
+
+elif n >= 120 and a < 18:
+    print("You are tall enough to ride the rollercoaster, A Ticket will cost you £7")
+
+else:
+    print("You are not tall enough to ride the rollercoaster! :(")
---
+++ Day 3/tickets.py
@@ -0,0 +1,17 @@
+print("Welcome to the rollercoaster!")
+
+height = int(input("What is your height in cm? \n"))
+age = int(input("How old are you? \n"))
+
+# if height >= 120:
+#     print("You can ride the rollercoaster!")
+# else:
+#     print("you are not tall enough to ride the rollercoaster")
+
+if height >= 120:
+    if age >= 18:
+        print("You are tall enough to ride the rollercoaster, Tickets cost $12")
+    else:
+        print("You are tall enough to ride the rollercoaster, Tickets cost $7")
+else:
+    print("You are not tall enough to ride the rollercoaster! :(")
