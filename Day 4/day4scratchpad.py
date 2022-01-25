9ef8f918453d03a095f5c13ab5ac341d5ed5fb9a
DiscPorridge <97075073+DiscoPorridge@users.noreply.github.com>
2022-01-25 03:41:13 +0000

    day 4

--- Day 4/day4scratchpad.py
+++ Day 4/day4scratchpad.py
@@ -2,16 +2,25 @@
 import random
 import my_module
 
-random_integer = random.randint(1, 100)
+# random_integer = random.randint(1, 100)
 
-print(random_integer)
+# print(random_integer)
 
-# print(my_module.test)
+# # print(my_module.test)
 
-# float1 = random.random(1, 10)
+# # float1 = random.random(1, 10)
 
-float1 = random.random() * 5
+# float1 = random.random() * 5
 
-float2 = random.uniform(1,10)
+# float2 = random.uniform(1,10)
 
-print(float1)
+# print(float1)
+
+num1 = ["1", "2", "3", "4", "5", "6", "7"]
+num2 = ["11", "12", "13", "14", "15", "16", "17"]
+
+# num1[-1] = "8"
+
+num3 = [num1, num2]
+
+print(num3[1][1])
