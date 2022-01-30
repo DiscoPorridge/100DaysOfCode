25ca19074cb3695ca1b0b69bee5e046fbd07e2c8
DiscoPorridge <97075073+DiscoPorridge@users.noreply.github.com>
2022-01-30 04:11:45 +0000

    day 5 start

--- Day 5/day5scratchpad.py
+++ Day 5/day5scratchpad.py
@@ -1,1 +1,9 @@
 ############## Day 5 Scratchpad ##############
+fruits = ["Apple", "Peach", "Pear"]
+
+# for loop - for x in y (operator z):
+
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " Pie")
+print(fruits)
