e61c5a2b4035fe8d9b5b1a8f9f56c3ac7a325527
DiscoPorridge <97075073+DiscoPorridge@users.noreply.github.com>
2022-01-31 03:08:29 +0000

    day 5 - scratchpad

--- Day 5/day5scratchpad.py
+++ Day 5/day5scratchpad.py
@@ -3,7 +3,28 @@ fruits = ["Apple", "Peach", "Pear"]
 
 # for loop - for x in y (operator z):
 
-for fruit in fruits:
-    print(fruit)
-    print(fruit + " Pie")
-print(fruits)
+# for fruit in fruits:
+#     print(fruit)
+#     print(fruit + " Pie")
+# print(fruits)
+
+
+# range()
+
+sum = 0
+
+for number in range(1, 101):
+    print(f"{sum} + {number} =")
+    sum += number
+    print(f"{sum}")
+print(f"total is {sum}")
+
+
+# range() with steps
+sum = 0
+
+for number in range(1, 101, 10):
+    print(f"{sum} + {number} =")
+    sum += number
+    print(f"{sum}")
+print(f"total is {sum}")
