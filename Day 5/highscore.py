7c76f37ca093f0db3e7884443e0647b2455acd71
DiscoPorridge <97075073+DiscoPorridge@users.noreply.github.com>
2022-01-31 02:43:47 +0000

    day 5 - high score

--- Day 5/highscore.py
+++ Day 5/highscore.py
@@ -11,11 +11,28 @@ print(student_scores)
 
 #Write your code below this row 👇
 
+# print maximum score in list
+print(f" {max(student_scores)} max()")
+
+# print minimum score in list
+print(f" {min(student_scores)} min()")
+
+# maximum score without max()
+
 # temp = 0
 max_score = 0
+min_score = 0
 
+# max score without max()
 for scores in student_scores:
     # temp = x
     if scores > max_score:
         max_score = scores
 print(f"The highest score in the class is: {max_score}")
+
+
+# terrible min score without min()
+for scores in student_scores:
+    if scores < student_scores[+1]:
+        min_score = scores
+print(f"minimum score {min_score}")
