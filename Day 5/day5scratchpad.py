52256cce6bc0cabad261e66eb00be204ffe49146
DiscoPorridge <97075073+DiscoPorridge@users.noreply.github.com>
2022-02-02 04:05:49 +0000

    day 5

--- Day 5/day5scratchpad.py
+++ Day 5/day5scratchpad.py
@@ -1,4 +1,12 @@
 ############## Day 5 Scratchpad ##############
+
+
+# imported modules
+
+import random
+
+
+# for loop list
 fruits = ["Apple", "Peach", "Pear"]
 
 # for loop - for x in y (operator z):
@@ -9,22 +17,47 @@ fruits = ["Apple", "Peach", "Pear"]
 # print(fruits)
 
 
-# range()
+# # range()
 
-sum = 0
+# sum = 0
 
-for number in range(1, 101):
-    print(f"{sum} + {number} =")
-    sum += number
-    print(f"{sum}")
-print(f"total is {sum}")
+# for number in range(1, 101):
+#     print(f"{sum} + {number} =")
+#     sum += number
+#     print(f"{sum}")
+# print(f"total is {sum}")
 
 
-# range() with steps
-sum = 0
+# # range() with steps
+# sum = 0
 
-for number in range(1, 101, 10):
-    print(f"{sum} + {number} =")
-    sum += number
-    print(f"{sum}")
-print(f"total is {sum}")
+# for number in range(1, 101, 10):
+#     print(f"{sum} + {number} =")
+#     sum += number
+#     print(f"{sum}")
+# print(f"total is {sum}")
+
+
+# fizzbuzz stuff 🤢
+
+# fizz = 0
+# buzz = 0
+
+# if buzz == 0:
+#     print("fizz")
+#     buzz = 1
+# if fizz == 0:
+#     print("buzz")
+#     fizz = 1
+
+# if fizz == 1 and buzz == 1:
+#     print("fizzbuzz")
+
+
+# password generator
+
+random.seed(random.randint(2197172813311000729512343216125642781, 90000000000000000006160000000000000000009))
+
+# print(random.random())
+
+# print(random.random())
