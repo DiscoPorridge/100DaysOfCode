3c73082e91f62f3873314ee597b43ace8a9eee21
DiscoPorridge <97075073+DiscoPorridge@users.noreply.github.com>
2022-02-05 04:11:36 +0000

    day 6 - blackjack

---
+++ Day 6/blackjack.py
@@ -0,0 +1,32 @@
+# import stuff
+
+
+
+# vars/lists (player/cpu hand, player,cpu value)
+
+
+# Welcome message
+
+
+# issue players first card and remove card from deck
+# issue players second card and remove card from deck
+
+# display cards to user
+
+# issue cpu face up card
+
+# display face up card and face down card
+
+# if user has blackjack, win game
+
+# ask if they want to stand or hit
+
+# loop draw new card and remove from deck if user hits until stand/bust
+
+# if user stand dealer show face down card
+
+# if blacjack, dealer wins
+
+# if dealer < 16 hit and remove from deck, if >= 17 stand,
+
+# display winner
